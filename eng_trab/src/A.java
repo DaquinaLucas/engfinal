3ed7aa52cdccdc65f0eca15e0e5c9e703dc7b26c
Lucas <logd15@hotmail.com>
2015-06-21 19:14:10 -0300

    Versão 1.3

--- eng_trab/src/A.java
+++ eng_trab/src/A.java
@@ -4,10 +4,13 @@ public class A {
 	int A1;
 	float A2;
 	String A3;
+	String Aramo4;
 	
 	public static float soma(int a, float b){
 		return (a+b);
 	}
+	
+	
 	
 		
 	public int getA1() {
@@ -27,5 +30,11 @@ public class A {
 	}
 	public void setA3(String a3) {
 		A3 = a3;
+	}
+	public String getAramo4() {
+		return Aramo4;
+	}
+	public void setAramo4(String aramo4) {
+		Aramo4 = aramo4;
 	}
 }
