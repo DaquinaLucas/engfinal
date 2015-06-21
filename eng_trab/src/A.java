4d4ebe64ebf858fc3807982f22ce2b12c2f6265f
Lucas <logd15@hotmail.com>
2015-06-21 18:57:16 -0300

    Adicionado String A3

--- eng_trab/src/A.java
+++ eng_trab/src/A.java
@@ -3,6 +3,8 @@ public class A {
 
 	int A1;
 	float A2;
+	String A3;
+	
 		
 	public int getA1() {
 		return A1;
@@ -15,5 +17,11 @@ public class A {
 	}
 	public void setA2(float a2) {
 		A2 = a2;
+	}
+	public String getA3() {
+		return A3;
+	}
+	public void setA3(String a3) {
+		A3 = a3;
 	}
 }
