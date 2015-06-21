2497fd9d72f68416afb96bbb086e58e1173345d3
Lucas <logd15@hotmail.com>
2015-06-21 20:23:22 -0300

    Versão 1.2

--- teste1/src/A.java
+++ teste1/src/A.java
@@ -3,6 +3,31 @@ public class A {
 
 	int A1;
 	float A2;
+	String A3;
+	String Aramo4;
+	String Aprinc4;
+	
+	public String getAramo4() {
+		return Aramo4;
+	}
+	public void setAramo4(String aramo4) {
+		Aramo4 = aramo4;
+	}
+	public String getAprinc4() {
+		return Aprinc4;
+	}
+	public void setAprinc4(String aprinc4) {
+		Aprinc4 = aprinc4;
+	}
+	public static float soma(int a, int b){
+		return (a+b);
+	}
+	public String getA3() {
+		return A3;
+	}
+	public void setA3(String a3) {
+		A3 = a3;
+	}
 	public int getA1() {
 		return A1;
 	}
