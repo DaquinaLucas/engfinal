352bedd337a8c3052a08700e77d1b6d6e709fd2b
Lucas <logd15@hotmail.com>
2015-06-21 19:30:29 -0300

    Versão 1.0

--- final_engenharia/src/A.java
+++ final_engenharia/src/A.java
@@ -1,10 +1,7 @@
 
 public class A {
-
 	int A1;
 	float A2;
-	String A3;
-	
 	public int getA1() {
 		return A1;
 	}
@@ -17,10 +14,5 @@ public class A {
 	public void setA2(float a2) {
 		A2 = a2;
 	}
-	public String getA3() {
-		return A3;
-	}
-	public void setA3(String a3) {
-		A3 = a3;
-	}
+	
 }
